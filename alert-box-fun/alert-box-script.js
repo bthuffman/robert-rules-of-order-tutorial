4537a03de1c42ec5ed14cb962771be638010eede
Brandon Huffman <bt_huffman@msn.com>
2019-06-06 18:40:03 -0600

    Minor changes to get functionality

--- alert-box-fun/alert-box-script.js
+++ alert-box-fun/alert-box-script.js
@@ -1,26 +1,23 @@
 function CustomAlert(){
-	//render method
-	this.render = function(){
+	this.render = function(dialog){
 		//window width and height variables to help position it in the direct center of screen.
-		let winW = window.innerWidth;
-		let winH = window.innerHeight;
-
-		let dialogoverlay = document.getElementById('dialogoverlay');
-		let dialogbox = document.getElementById('dialogbox');
-	//	make the overlay seen.
+		var winW = window.innerWidth;
+		var winH = window.innerHeight;
+		var dialogoverlay = document.getElementById('dialogoverlay');
+		var dialogbox = document.getElementById('dialogbox');
+		//	make the overlay seen.
 		dialogoverlay.style.display = "block";
 		dialogoverlay.style.height = winH+"px";
-		dialogbox.style.left = (winW/2) - (550 * 0.5) + "px";
+		dialogbox.style.left = (winW/2) - (550 * .5)+"px";
 		dialogbox.style.top = "100px";
 		dialogbox.style.display = "block";
 		document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
 		document.getElementById('dialogboxbody').innerHTML = dialog;
 		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
-	}
-	//ok method
+	};
 	this.ok = function(){
-
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
 	}
 }
-
-let Alert = new CustomAlert();
+var Alert = new CustomAlert();
